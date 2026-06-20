593e4cdcc5fe7ed67d2cd9b1330a8bdb558426d8
Vojtech Kylar <vojtech@vkylar.com>
2026-06-20 11:12:05 +0200

    feat: add trigger to create invoice_history on invoice update

---
+++ drizzle/0001_add_invoice_history_on_update_trigger.sql
@@ -0,0 +1,30 @@
+-- Custom SQL migration file, put your code below! --
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE OR REPLACE FUNCTION create_invoice_history_on_update()
+RETURNS trigger AS $$
+BEGIN
+  INSERT INTO "delivry-task_invoices_history" (
+    "id",
+    "shipmentId",
+    "weight",
+    "price",
+    "createdAt",
+    "updatedAt"
+  ) VALUES (
+    OLD."id",
+    OLD."shipmentId",
+    OLD."weight",
+    OLD."price",
+    OLD."createdAt",
+    OLD."updatedAt"
+  );
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER invoices_history_on_update_trigger
+AFTER UPDATE ON "delivry-task_invoices"
+FOR EACH ROW
+EXECUTE FUNCTION create_invoice_history_on_update();
